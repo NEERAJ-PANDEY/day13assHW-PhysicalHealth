37049dc4f66963a9218d08f9822d67a06f281a42
NEERAJ-PANDEY <neerajpandey007star@gmail.com>
2023-11-26 22:21:25 +0530

    day13hw

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Banner from './components/Banner';
+import Exercise from './components/Exercise';
+
+function App() {
+  return (
+   <div>
+      <>
+     <Banner />
+     <Exercise />
+     </>
+   </div>
+  );
+}
+
+export default App;
